1c714d1ba99624c5d1e5c25c2b1b9b9bba765d4f
Winston Pacheco Junior <winston@atleastonce.cc>
2024-09-15 11:14:00 -0300

    turn integration calls async, create Weather entity and tests

--- cmd/temperature/main.go
+++ cmd/temperature/main.go
@@ -1,1 +1,27 @@
 package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/winstonjr/goexpert-desafio-cloudrun/internal/infra/integration"
+	"github.com/winstonjr/goexpert-desafio-cloudrun/internal/usecase"
+	"os"
+)
+
+func main() {
+	viacepIntegration := integration.NewViacepIntegration()
+	weatherapiIntegration := integration.NewWeatherapiIntegration("23b020471bba461680101942241309")
+	checkWeatherUseCase := usecase.NewCheckWeatherUseCase(weatherapiIntegration, viacepIntegration)
+	for _, cep := range os.Args[1:] {
+		temperature, err := checkWeatherUseCase.Execute(cep)
+		if err != nil {
+			panic(err)
+		}
+
+		data, err := json.Marshal(temperature)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Erro ao realizar parse do json: %v\n", err)
+		}
+		fmt.Println(string(data))
+	}
+}
